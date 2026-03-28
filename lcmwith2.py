1f09b996660d3d3061afd7b2e05aef3a9c29f29a
Aki30312 <aklesiafekadeselassie@gmail.com>
2026-03-28 23:05:56 +0300

    Implement smallestEvenMultiple method in Solution class

---
+++ lcmwith2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+         num = 2
+         n >=2
+         if n%2 != 0:
+            return n*2 
+         else:
+            return n
